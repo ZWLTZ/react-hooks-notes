498526a7046ae740fa89bf10bb347094da28bd9b
JianFei.Wang <jiucheng0605@gmail.com>
2019-10-21 16:53:32 +0800

    useEffect对应的生命周期

--- src/examples/hooks/UseEffect.jsx
+++ src/examples/hooks/UseEffect.jsx
@@ -4,14 +4,14 @@ import styles from '../Example.module.scss'
 
 const TestUnMounted = () => {
   useEffect(() => {
-    console.log(`useEffect---->component mounted and updated.`)
+    console.log(`Child: useEffect-->component mounted and updated.`)
     return () => {
       // return 会与：willUnMounted
-      console.log(`useEffect---->component will unmounted.`)
+      console.log(`Child: useEffect---->component will unmount.`)
     };
   }, [])
   return (
-    <div>哈哈哈哈</div>
+    <div>哈哈哈哈,useEffect可以代替componentDidMount和willUnMount</div>
   )
 }
 // 2、Hooks-useEffect
@@ -21,12 +21,12 @@ const UseEffectExample = () => {
   // 1、代替了：componentDidMount和componentDidUpdate  lifecycle
   // 2、异步的
   useEffect(() => {
-    console.log(`useEffect---->${show}`)
+    console.log(`Parent: useEffect-->${show}-mounted and updated.`)
   })
   return (
     <div className={styles.main}>
       <p>You clicked {count} times.</p>
-      <button onClick={() => { setCount(count + 1) }}>点击</button>
+      <button onClick={() => { setCount(count + 1) }}>增加</button>
       <button onClick={() => { setShow(!show) }}>{show ? '隐藏' : '显示'}</button>
       {
         show ? <TestUnMounted /> : null
